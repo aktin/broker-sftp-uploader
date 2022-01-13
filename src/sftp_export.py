0c517d2a6add1e6c19d66aa6b7ef49dda5183c4f
Alexander Kombeiz <alexkombie@gmail.com>
2022-01-13 12:47:49 +0100

    added license and copyright

--- src/sftp_export.py
+++ src/sftp_export.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 15 16:11:47 2021
-@author: Alexander Kombeiz (akombeiz@ukaachen.de)
-"""
-
+# Created on Thu Jul 15 16:11:47 2021
 # @VERSION=1.0
+
+#
+#      Copyright (c) 2022  Alexander Kombeiz
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU Affero General Public License as
+#      published by the Free Software Foundation, either version 3 of the
+#      License, or (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU Affero General Public License for more details.
+#
+#      You should have received a copy of the GNU Affero General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
 
 import sys
 import json
@@ -18,6 +32,9 @@ import paramiko
 from datetime import datetime
 from cryptography.fernet import Fernet
 
+
+# TODO outsource encryption to openssl
+# TODO set encryption to asymmetrical
 
 class BrokerRequestResultManager:
 
@@ -157,7 +174,6 @@ class SftpFileManager:
     def __extract_filename_from_broker_response(response: requests.models.Response) -> str:
         return re.search('filename=\"(.*)\"', response.headers['Content-Disposition']).group(1)
 
-    # TODO outsource encryption to openssl
     def __encrypt_file(self, file: bytes) -> bytes:
         return self.ENCRYPTOR.encrypt(file)
 
