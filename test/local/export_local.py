3a1c9339ea73d67f868409db5f12c9bfb49ff112
Alexander Kombeiz <alexkombie@gmail.com>
2023-03-02 10:24:02 +0100

    adjusted local execution for toml

--- test/local/export_local.py
+++ test/local/export_local.py
@@ -9,7 +9,7 @@ sys.path.insert(0, path_src)
 
 import sftp_export
 
-path_settings = os.path.join(this_path.parents[1], 'resources', 'settings.json')
+path_settings = os.path.join(this_path.parents[1], 'resources', 'settings.toml')
 start_time = time.time()
 sftp_export.main(path_settings)
 print("--- %s seconds ---" % (time.time() - start_time))
