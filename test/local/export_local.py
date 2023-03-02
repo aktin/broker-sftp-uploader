57e19898e8fdd9d685f813902c0fb5feeccbc6c6
Alexander Kombeiz <alexkombie@gmail.com>
2023-03-02 09:25:13 +0100

    adjusted local testing script, moved resources into own folder

--- test/local/export_local.py
+++ test/local/export_local.py
@@ -1,7 +1,15 @@
 import os
-import src.sftp_export
+import sys
+import time
+from pathlib import Path
 
-path_parent = os.path.dirname(os.getcwd())
-path_local = os.path.join(path_parent, 'local')
-path_settings = os.path.join(path_local, 'settings.json')
-src.sftp_export.main(path_settings)
+this_path = Path(os.path.realpath(__file__))
+path_src = os.path.join(this_path.parents[2], 'src')
+sys.path.insert(0, path_src)
+
+import sftp_export
+
+path_settings = os.path.join(this_path.parents[1], 'resources', 'settings.json')
+start_time = time.time()
+sftp_export.main(path_settings)
+print("--- %s seconds ---" % (time.time() - start_time))
